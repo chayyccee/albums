7347a774b7abb45de959740cf4766d041a957ec2
Eke Chinedu <ekechinedu488@gmail.com>
2020-09-28 15:33:34 +0100

    last part

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
     const { 
         buttonStyle,
         textStyle
@@ -10,7 +10,7 @@ const Button = ({ onPress }) => {
     return (
         <TouchableOpacity onPress={onPress} style={buttonStyle}>
             <Text style={textStyle}>
-                Click Me
+                {children}
             </Text>
         </TouchableOpacity>
     );
